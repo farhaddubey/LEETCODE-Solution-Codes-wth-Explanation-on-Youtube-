05fe622539f868b0bfa7e5191d1eab60af0bbb96
farhaddubey <farhaddubey003@gmail.com>
2024-06-05 09:44:39 +0530

    l

---
+++ leetcode2visualisation.java
@@ -0,0 +1,27 @@
+Before:    l1: 2 -> 4 -> 3
+                  \
+                   -> 5 -> 6 -> 4 :l2
+dummyHead: 0 -> null
+
+Before:    l1: 2 -> 4 -> 3
+                  \
+                   -> 5 -> 6 -> 4 :l2
+dummyHead: 0 -> null
+curr: null
+
+Before:    l1: 2 -> 4 -> 3
+                  \
+                   -> 5 -> 6 -> 4 :l2
+dummyHead: 0 -> null
+curr: 0 -> null
+
+Before:    l1: 2 -> 4 -> 3
+                  \
+                   -> 5 -> 6 -> 4 :l2
+dummyHead: 0 -> null
+curr: 0 -> null
+carry: 0
+
+Before:    l1: 2 -> 4 -> 3
+                  \
+                   -> 5 -> 
